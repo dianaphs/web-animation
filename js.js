58f011d34f01850fb8a178c0c58404594ad2d2bd
Diana <dianaph-nguyen@hotmail.com>
2019-05-23 15:10:54 +0200

    javascript toegevoegd

---
+++ js.js
@@ -0,0 +1,61 @@
+document.querySelectorAll("#element1, #element2, #element3, #element4, #element5, #element6, #element7, #element8, #element9, #element10, #element11, #patroon, #rits, #rits2, #ball1, #ball2, #ball3").forEach(function (element) {
+    element.addEventListener("click", function () {
+        element.classList.toggle("is-active");
+    });
+});
+
+document.querySelectorAll('#ball1, #ball2').forEach(function (element) {
+    element.addEventListener('click', function () {
+        console.log(element);
+        element.classList.toggle('is-active');
+    })
+})
+
+document.onkeyup = function (e) {
+    if (e.keyCode === 65) {
+        document.getElementById("element1").classList.toggle("is-active");
+    }
+    if (e.keyCode === 66) {
+        document.getElementById("element2").classList.toggle("is-active");
+    }
+    if (e.keyCode === 67) {
+        document.getElementById("element3").classList.toggle("is-active");
+    }
+    if (e.keyCode === 68) {
+        document.getElementById("rits").classList.toggle("is-active");
+    }
+    if (e.keyCode === 69) {
+        document.getElementById("ball").classList.toggle("is-active");
+    }
+    if (e.keyCode === 70) {
+        document.getElementById("element6").classList.toggle("is-active");
+    }
+    if (e.keyCode === 71) {
+        document.getElementById("element7").classList.toggle("is-active");
+    }
+    if (e.keyCode === 72) {
+        document.getElementById("patroon").classList.toggle("is-active");
+    }
+    if (e.keyCode === 73) {
+        document.getElementById("element9").classList.toggle("is-active");
+    }
+    if (e.keyCode === 74) {
+        document.getElementById("element10").classList.toggle("is-active");
+    }
+    if (e.keyCode === 75) {
+        document.getElementById("element11").classList.toggle("is-active");
+    }
+};
+
+//document.onkeyup = function (e) {
+//    if (e.keyCode === 68) {
+//        document.querySelectorAll("#rits2, #rits1").classList.toggle("is-active");
+//    }
+//};
+//
+//var animate = document.getElementById("rits2");
+//document.addEventListener('keypress', function (e) {
+//    if (e.keyCode === 68) {
+//        animate.classList.toggle("is-active");
+//    }
+//})
